6448a6f82f96899104efd956eb28d70d768d1acb
Yasser <yasser304@gmail.com>
2013-07-01 16:27:17 -0400

    Ebay app using jQuery, UI only

---
+++ EbayjQueryProj/apps/EbayjQuery/android/native/assets/www/default/wlclient/js/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":1699438966,"date":1372708845821,"machine":"NYACONE430YM"};
+/* Date: Mon Jul 01 16:00:45 EDT 2013 */
---
+++ EbayjQueryProj/apps/EbayjQuery/common/js/EbayjQuery.js
@@ -0,0 +1,64 @@
+/*
+* Licensed Materials - Property of IBM
+* 5725-G92 (C) Copyright IBM Corp. 2006, 2013. All Rights Reserved.
+* US Government Users Restricted Rights - Use, duplication or
+* disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+*/
+
+function wlCommonInit(){
+
+	/*
+	 * Application is started in offline mode as defined by a connectOnStartup property in initOptions.js file.
+	 * In order to begin communicating with Worklight Server you need to either:
+	 * 
+	 * 1. Change connectOnStartup property in initOptions.js to true. 
+	 *    This will make Worklight framework automatically attempt to connect to Worklight Server as a part of application start-up.
+	 *    Keep in mind - this may increase application start-up time.
+	 *    
+	 * 2. Use WL.Client.connect() API once connectivity to a Worklight Server is required. 
+	 *    This API needs to be called only once, before any other WL.Client methods that communicate with the Worklight Server.
+	 *    Don't forget to specify and implement onSuccess and onFailure callback functions for WL.Client.connect(), e.g:
+	 *    
+	 *    WL.Client.connect({
+	 *    		onSuccess: onConnectSuccess,
+	 *    		onFailure: onConnectFailure
+	 *    });
+	 *     
+	 */
+	
+	
+	// Common initialization code goes here
+
+}
+
+var lat;
+var lng; 
+
+navigator.geolocation.getCurrentPosition (function (pos)
+		{
+		  lat = pos.coords.latitude;
+		  lng = pos.coords.longitude;
+		});
+
+		$("#btn").bind ("click", function (event)
+		{
+		  //var lat = $("#lat").val ();
+		  //var lng = $("#lng").val ();
+		  var latlng = new google.maps.LatLng (lat, lng);
+		  var options = { 
+		    zoom : 15, 
+		    center : latlng, 
+		    mapTypeId : google.maps.MapTypeId.ROADMAP 
+		  };
+		  var $content = $("#map div:jqmData(role=content)");
+		  $content.height (screen.height - 50);
+		  var map = new google.maps.Map ($content[0], options);
+		  $.mobile.changePage ($("#map"));
+		  
+		  new google.maps.Marker ( 
+		  { 
+		    map : map, 
+		    animation : google.maps.Animation.DROP,
+		    position : latlng  
+		  });  
+		});
